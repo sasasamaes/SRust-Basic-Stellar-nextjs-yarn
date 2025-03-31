651fcd0d576683b1f12437285da50cc07c0a1f5f
Francisco Campos Díaz <sasasamaes@users.noreply.github.com>
2025-03-30 18:10:04 -0600

    delete comments

--- packages/soroban/example-contract/test.rs
+++ packages/soroban/example-contract/test.rs
@@ -1,20 +1,18 @@
 #[cfg(test)]
 mod test {
-    use super::*; // Importa el código del contrato
+    use super::*; 
     use soroban_sdk::{symbol_short, Env};
 
     #[test]
     fn test_hello() {
-        let env = Env::default(); // Crea un entorno de test
+        let env = Env::default();
         let contract_id = env.register_contract(None, ExampleContract);
         let client = ExampleContractClient::new(&env, &contract_id);
 
         let input = symbol_short!("friend");
-        let expected_output = symbol_short!("Hello"); // Simplificado
+        let expected_output = symbol_short!("Hello");
 
         let result = client.hello(&input);
         assert_eq!(result, expected_output);
     }
 }
-
-// Si usas este método, el archivo `test.rs` que creaste puede quedar vacío o ser eliminado.
